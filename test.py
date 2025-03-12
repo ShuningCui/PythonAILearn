74bfc7cb16df2a4a8fdc30b5531791e2a9e5e21e
Shuning Cui <cuishuning@outlook.com>
2025-03-12 20:19:10 +0800

    kjnkll

--- test.py
+++
@@ -1,7 +0,0 @@
-x = [i for i in input().upper() if i.isalpha()]
-y = []
-for s in range(26):
-    y.append((chr(ord('A')+s),x.count(chr(ord('A')+s))))
-y.sort(key = lambda x:x[1],reverse = True)
-z = [f'{t[0]}-{t[1]}' for t in y]
-print(*z)
